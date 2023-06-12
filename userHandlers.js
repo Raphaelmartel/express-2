889ea824707491f02505d8a54c98f4a3f2801d3b
Raphaël Martel <contact@raphaelmartel.com>
2023-06-12 11:43:31 +0200

    delete

--- userHandlers.js
+++ userHandlers.js
@@ -70,11 +70,32 @@ const updateUser = (req,res) => {
   });
 }
 
+const deleteUser = (req,res) => {
+  const id = parseInt(req.params.id);
+
+  database
+  .query(
+    "delete from users where id = ?", [id],
+  )
+  .then(([result]) => {
+    if (result.affectedRows === 0) {
+      res.status(404).send("Not Found");
+    } else {
+      res.sendStatus(204);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error editing the user");
+  });
+}
+
 module.exports = {
   getUsers,
   getUserById,
   postUser, // don't forget to export your function ;)
   updateUser,
+  deleteUser,
 };
 
 
